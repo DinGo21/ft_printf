b7417771e67ae823d84307e6c3f361b4dae54510
Diego Antonio Santamaria Makso <disantam@c3r1s4.42malaga.com>
2024-01-19 16:26:53 +0100

    some minor fixes

--- ft_dec.c
+++ ft_dec.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_dec.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: disantam <disantam@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:35:54 by disantam          #+#    #+#             */
-/*   Updated: 2023/06/23 17:30:19 by disantam         ###   ########.fr       */
+/*   Updated: 2024/01/17 12:14:54 by disantam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ static int	ft_itoa_len(long n)
 
 	len = 0;
 	if (n <= 0)
+	{
 		len++;
+	}
 	while (n != 0)
 	{
 		n /= 10;
@@ -35,10 +37,14 @@ static char	*ft_itoa(long n)
 	len = ft_itoa_len(n);
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
+	{
 		return (NULL);
+	}
 	str[len--] = '\0';
 	if (n == 0)
+	{
 		str[0] = '0';
+	}
 	if (n < 0)
 	{
 		str[0] = '-';
@@ -58,7 +64,9 @@ static int	ft_uitoa_len(size_t n)
 
 	len = 0;
 	if (n == 0)
+	{
 		len++;
+	}
 	while (n != 0)
 	{
 		n /= 10;
@@ -75,10 +83,14 @@ static char	*ft_uitoa(size_t n)
 	len = ft_uitoa_len(n);
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
+	{
 		return (NULL);
+	}
 	str[len--] = '\0';
 	if (n == 0)
+	{
 		str[0] = '0';
+	}
 	while (n > 0)
 	{
 		str[len--] = (n % 10) + '0';
@@ -93,12 +105,18 @@ int	ft_dec(long n, char f)
 	char	*str;
 
 	if (f == 'd' || f == 'i')
+	{
 		str = ft_itoa(n);
+	}
 	else
+	{
 		str = ft_uitoa(n);
+	}
 	c = -1;
 	while (str[++c] != '\0')
+	{
 		write(1, str + c, 1);
+	}
 	free(str);
 	return (c);
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: disantam <disantam@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:43:50 by disantam          #+#    #+#             */
-/*   Updated: 2023/11/09 10:52:52 by disantam         ###   ########.fr       */
+/*   Updated: 2024/01/17 12:12:29 by disantam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,13 @@ int	ft_printf(char const *s, ...)
 	while (s[i] != '\0')
 	{
 		if (s[i] != '%')
+		{
 			c += write(1, s + i, 1);
+		}
 		else
+		{
 			c += ft_printf_check(s[++i], args);
+		}
 		i++;
 	}
 	va_end(args);
